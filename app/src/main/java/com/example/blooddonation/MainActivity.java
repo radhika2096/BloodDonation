d0e06d1ddc06364bc91882da36486825718e2991
chetan9373 <61279954+chetan9373@users.noreply.github.com>
2022-03-05 15:12:33 +0530

    V V XZVC XV

--- app/src/main/java/com/example/blooddonation/MainActivity.java
+++ app/src/main/java/com/example/blooddonation/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.blooddonation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
-
+    Animation animation;
+    View img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img =(View)findViewById(R.id.image);
+        animation= AnimationUtils.loadAnimation(this,R.anim.zoom);
+        img.setAnimation(animation);
     }
 }
