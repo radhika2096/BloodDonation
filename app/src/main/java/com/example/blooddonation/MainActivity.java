916c136c9b91af7715201952adc7d0b287c3b0ae
chetan9373 <61279954+chetan9373@users.noreply.github.com>
2022-03-12 21:44:04 +0530

    chetan

--- app/src/main/java/com/example/blooddonation/MainActivity.java
+++ app/src/main/java/com/example/blooddonation/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+{
     Animation animation;
     View img;
     @Override
@@ -17,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
         img =(View)findViewById(R.id.image);
         animation= AnimationUtils.loadAnimation(this,R.anim.zoom);
         img.setAnimation(animation);
+        Spinner spinner = findViewById(R.id.Gender);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Gender, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
+
